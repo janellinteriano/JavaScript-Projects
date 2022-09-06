28bff45b6fd957e22a92009fefc5385a1ebcc1e2
janellinteriano <111891542+janellinteriano@users.noreply.github.com>
2022-09-06 14:26:03 -0400

    Added an index and JS page

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,8 @@
+window.alert("Welcome to my page!"); //Utilized the window.alert() method
+
+var Sent1 = "This is the beginning of the string" //Defined a variable and gave it a string value
+var Sent2 = " and this is the end of the string <br>" //Defined a variable and gave it a string value
+
+document.write(Sent1 + Sent2); //Concatenated the Sent1 and Sent2 variables
+
+document.write(4 + 4); //Wrote an expression
